54911f4081545094dcf66173c69976671d08d948
Killacan <kameron000@yahoo.com>
2022-07-07 14:53:52 -0700

    finished the game

--- TDD_project/lib/problems.rb
+++ TDD_project/lib/problems.rb
@@ -25,3 +25,97 @@ class Array
         new_arr
     end
 end
+
+def my_transpose(arr) 
+    new_arr = []
+
+    arr.each_with_index do |ele, i|
+        temp_arr = []
+        (0 ... ele.length).each do |j| 
+            temp_arr << arr[j][i] 
+        end
+        new_arr << temp_arr
+    end
+    new_arr
+    
+end
+
+def stock_picker(arr)
+    most_profitable_days = [0,0]
+    profit = 0
+
+    arr.each_with_index do |price, i|
+        (i + 1 ... arr.length).each do |j|
+            if arr[j] - arr[i] > profit
+                most_profitable_days = [i, j]
+                profit = arr[j] - arr[i]
+            end
+        end
+    end
+    most_profitable_days
+end
+
+class TowersOfHanoi
+
+    attr_reader :array1, :array2, :array3
+    # each array can be its own number. 
+    def initialize
+        @array1 = [3,2,1]
+        @array2 = []
+        @array3 = []
+
+    end
+
+    def move(arr1, arr2) 
+        raise "not valid" if !valid_move?(arr1, arr2)
+        arr2 << arr1.pop
+
+    end
+
+    def valid_move?(arr1, arr2)
+        raise "stack is empty, pick another stack" if arr1.empty?
+        disk = arr1[-1] 
+        return true if  arr2.empty? || disk < arr2[-1]
+        false
+    end
+
+    def won? 
+        @array3 == [3,2,1]
+        puts " YOU WON!"
+    end
+
+    def play
+
+        while !won? 
+            begin
+            self.renderer 
+            puts " enter two numbers, tower moving from and tower moving to. ex: 2 3"
+            input = gets.chomp.split(" ")
+            input.map! {|ele| ele.to_i}
+            
+            move(array_picker(input.first), array_picker(input.last))
+            rescue RuntimeError
+                puts "its not valid" 
+            end
+        end
+    end
+
+    def array_picker(num)
+        if num == 1
+            @array1
+        elsif num == 2
+            @array2
+        else
+            @array3
+        end
+    end
+
+    def renderer
+        p @array1 
+        p @array2
+        p @array3
+    end
+end
+
+tower = TowersOfHanoi.new
+tower.play
--- TDD_project/spec/problems_spec.rb
+++ TDD_project/spec/problems_spec.rb
@@ -12,3 +12,22 @@ describe "Array#two_sum" do
         expect( [-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
     end
 end
+
+describe "my_transpose" do 
+    it "should convert between the row and the column" do 
+        my_trans = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8]
+          ]
+          expect(my_transpose(my_trans)).to eq([[0, 3, 6], [1, 4, 7], [2, 5, 8]])
+
+    end
+end
+
+describe "#stock_picker" do 
+    it "buy low sell high" do
+        stock_price = [2,1,3,1,9]
+        expect(stock_picker(stock_price)).to eq([1,4])
+    end
+end
